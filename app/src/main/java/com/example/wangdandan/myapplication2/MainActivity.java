626e9aee58f6e24677b42645e5da92507fe311b4
wangdandan <15001092547@163.com>
2017-03-22 15:25:55 +0800

    修改

--- app/src/main/java/com/example/wangdandan/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/wangdandan/myapplication2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.wangdandan.myapplication2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent i = new Intent();
     }
 }
